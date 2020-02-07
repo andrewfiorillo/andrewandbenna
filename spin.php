cfbea0ce5b557ef0dec31898e57386daa874effc
andrewfiorillo <fiorilloaj@gmail.com>
2020-02-07 00:20:37 -0500

    image spinning

---
+++ spin.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<?
+		$page = "home";
+		$page_title = "Home";
+	?>
+		
+	<? include("include/header.php"); ?>
+</head>
+<body>
+	
+	<? include("include/nav.php") ?>
+
+	<div class="illustration-wrapper">
+		<div class="illustrations">
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+			<div class="illustration no-rotation"></div>
+		</div>
+		<div class="illustrations-content">
+			<span>
+				7<span class="dot">•</span>11<span class="dot">•</span>2020
+				<br />
+				Harvard, MA
+				<br />
+				rsvp
+			</span>
+		</div>
+	</div>
+
+	<!-- <div class="main" style="max-width:440px;">
+		<div class="hero-home">
+			<div class="home-info">
+				<h2>7<span class="dot">•</span>11<span class="dot">•</span>2020</h2>
+				<h2>Harvard, MA</h2>
+				<h2><a href="/rsvp">rsvp</a></h2>
+			</div>
+		</div>
+	</div> -->
+
+</body>
+</html>
