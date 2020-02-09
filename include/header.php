697b86db6bbc62101d99e4e72907f2cb32192092
andrewfiorillo <fiorilloaj@gmail.com>
2020-02-09 13:47:11 -0500

    bust cache

--- include/header.php
+++ include/header.php
@@ -4,4 +4,4 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	
 	<!-- <link href="https://fonts.googleapis.com/css?family=Amatic+SC:700|Puritan:400,400i,700&display=swap" rel="stylesheet"> -->
-	<link rel='stylesheet' href='/assets/style/style.css?v=1' />
+	<link rel='stylesheet' href='/assets/style/style.css?v=2' />
