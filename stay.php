a6514e8a852cbb75bf5c5cdc6475a02039251f29
andrewfiorillo <fiorilloaj@gmail.com>
2020-02-10 00:07:12 -0500

    updated content, adjusted spacing

--- stay.php
+++ stay.php
@@ -15,21 +15,29 @@
 		<h2>On-Site Lodging</h2>
 		<p>There are a limited number of rooms at the venue itself, and we’d love to get as many of you on-site as we can. It’s pretty rustic and not quite as luxurious as a hotel, but it is cheap and super convenient. Some rooms are private and some are shared, with 2, 3, 4, and 6 or more beds in a room. If you are cool with sharing a room, we can put you up for free! If you claim one of the more private (3 or less), it is $50 per guest per night. Let us know if you’re interested!</p>
 
-		<h2>Hotels w/ Wedding block</h2>
+		<h2>Hotels</h2>
+		<p>We have secured rooms at several hotels in the area. If you book with one of the following hotels, it’ll be a better bet for availability, and it will be discounted. Let us know if you have any questions</p>
 		<p>
-			Marriott Residence Inn<br>
+			<strong>Marriott Residence Inn</strong><br>
 			Address: 7 Lan Dr, Westford, MA 01886<br>
-			Rate: 1 bed/1 bath - $150, 2 bed/2 bath - $199<br>
-			Wedding Block Name: eatingpants<br>
-			<br>
-			Hampton Inn<br>
+			Group Name: Eating Pants Wedding<br>
+			Rates:<br>
+			Queen Suite $149/night<br>
+			King Suite $159/night<br>
+			Two Bedroom Suite $199/night<br>
+			The last day to book this hotel is Tuesday, June 9, 2020.<br>
+			<a href="#">Book here</a><br>
+		</p>
+		<p>
+			<strong>Hampton Inn</strong><br>
 			Address: 9 Nixon Rd, Westford, MA 01886<br>
 			Phone (978) 392-1555<br>
 			Room Types: 2 Queen Beds/1 King Bed<br>
 			Rate: 2 Queen - $119, 1 king - $119<br>
-			Wedding Block Name: eatingpants<br>
-			<br>
-			Westford Regency<br>
+			Wedding Block Name: eatingpants
+		</p>
+		<p>
+			<strong>Westford Regency</strong><br>
 			Address: 219 Littleton Rd, Westford, MA 01886<br>
 			Phone: (978) 692-8200<br>
 			Rate: 2 Queen - $119, 1 king - $119<br>
