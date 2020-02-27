35cda34771f6e2b8c1555997aaeb5649408063b3
andrewfiorillo <fiorilloaj@gmail.com>
2020-02-26 21:23:46 -0500

    added images and bumped css version

--- include/header.php
+++ include/header.php
@@ -1,4 +1,4 @@
 	<title>Andrew & Benna | <?= $page_title ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<link rel='stylesheet' href='/assets/style/style.css?v=3.3' />
+	<link rel='stylesheet' href='/assets/style/style.css?v=3.4' />
