2b0f9c6a0ffa847b698fa1eb4e6aa627382f3085
andrewfiorillo <fiorilloaj@gmail.com>
2020-04-25 14:05:17 -0400

    updated copy

--- stay.php
+++ stay.php
@@ -28,28 +28,32 @@
 		</div>
 
 		<h2>reserved hotels</h2>
-		<p>We have blocked rooms at several hotels in the area. If you book with one of the following hotels, it’ll be a better bet for availability, and it will be discounted. Let us know if you have any questions.</p>
+		<p>
+			<strong>UPDATE:</strong> Hold off on booking these if you want the reduced rate. Since we postponed the wedding, we are sorting out new hotel arrangements.
+			<br><br>
+			We have blocked rooms at several hotels in the area. If you book with one of the following hotels, it’ll be a better bet for availability, and it will be discounted. Let us know if you have any questions.
+		</p>
 		
 		<h3>Marriott Residence Inn</h3>
 		<p>
 			Address: <a href="https://goo.gl/maps/Pe8eRNrnykaMmM6S7" target="_blank">7 Lan Dr, Westford, MA 01886</a><br>
 			Phone: (978) 392-1407<br>
-			Rates: $149 - $199 per night<br>
-			Group Name: Eating Pants Wedding<br>
+			<!-- Rates: $149 - $199 per night<br> -->
+			<!-- Group Name: Eating Pants Wedding<br> -->
 			<!-- Queen Suite $149/night<br>
 			King Suite $159/night<br>
 			Two Bedroom Suite $199/night<br> -->
-			The last day to book is Tuesday, June 9, 2020.<br>
-			<a href="https://www.marriott.com/events/start.mi?id=1580847691835&key=GRP" target="_blank">Book here</a>
+			<!-- The last day to book is Tuesday, June 9, 2020.<br> -->
+			<!-- <a href="https://www.marriott.com/events/start.mi?id=1580847691835&key=GRP" target="_blank">Book here</a> -->
 		</p>
 
 		<h3>Hampton Inn</h3>
 		<p>
 			Address: <a href="https://goo.gl/maps/eoWYbhMRETGe6k8BA" target="_blank">9 Nixon Road, Westford, MA 01886</a><br>
 			Phone: (978) 392-1555<br>
-			Rates: $129 - $141 per night<br>
-			Group Name: Eating Pants<br>
-			<a href="https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fhamptoninn.hilton.com%2Fen%2Fhp%2Fgroups%2Fpersonalized%2FB%2FBOSWFHX-EPB-20200709%2Findex.jhtml%3FWT.mc_id%3DPOG&data=02%7C01%7Camber.thompson2%40hilton.com%7Cf4cf6deb4af14b239f7808d7aa3c3223%7C660292d2cfd54a3db7a7e8f7ee458a0a%7C0%7C0%7C637165047826371423&sdata=h4dbC6G14EWI8MQsVXIGDgeFxb77L4%2BEB8le1OBcgSs%3D&reserved=0" target="_blank">Book here</a>
+			<!-- Rates: $129 - $141 per night<br> -->
+			<!-- Group Name: Eating Pants<br> -->
+			<!-- <a href="https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fhamptoninn.hilton.com%2Fen%2Fhp%2Fgroups%2Fpersonalized%2FB%2FBOSWFHX-EPB-20200709%2Findex.jhtml%3FWT.mc_id%3DPOG&data=02%7C01%7Camber.thompson2%40hilton.com%7Cf4cf6deb4af14b239f7808d7aa3c3223%7C660292d2cfd54a3db7a7e8f7ee458a0a%7C0%7C0%7C637165047826371423&sdata=h4dbC6G14EWI8MQsVXIGDgeFxb77L4%2BEB8le1OBcgSs%3D&reserved=0" target="_blank">Book here</a> -->
 		</p>
 
 
